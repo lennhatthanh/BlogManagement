09c9eaac5bc7e15fec4f3be8b4e3032608fea2ed
Lê Nguyễn Nhật Thành <lenguyennhatthanh72@gmail.com>
2025-11-07 10:00:50 +0700

    Format published date dynamically in BlogDetail component

--- src/pages/BlogDetail/index.jsx
+++ src/pages/BlogDetail/index.jsx
@@ -12,24 +12,29 @@ export default function BlogDetail() {
         const res = await getBlog(id);
         setBlogs(res.data);
     };
+    const date = new Date(blog.updatedAt);
+
+    const formatted = date.toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    });
     return (
         <div>
             <div className="text-center">
-                <p className="text-primary font-medium">Published on October 3, 2025</p>
+                <p className="text-primary font-medium">Published on {formatted}</p>
                 <h1 className="text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto! py-4">{blog.title}</h1>
                 <p className="inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-primary">
                     {blog.author?.username}
                 </p>
             </div>
             <div className="mx-5 max-w-5xl md:mx-auto my-10 mt-6">
-                <img
-                    alt=""
-                    className="rounded-3xl mb-5 mx-auto"
-                    src={blog.image}
-                />
+                <img alt="" className="rounded-3xl mb-5 mx-auto" src={blog.image} />
             </div>
-            <div className="blog-details rich-text max-w-3xl mx-auto px-4 text-left text-foreground" dangerouslySetInnerHTML={{ __html: blog.content }} >
-            </div>
+            <div
+                className="blog-details rich-text max-w-3xl mx-auto px-4 text-left text-foreground"
+                dangerouslySetInnerHTML={{ __html: blog.content }}
+            ></div>
         </div>
     );
 }
